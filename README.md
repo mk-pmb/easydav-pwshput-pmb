c7a35e70a4d08ee3778372db3ae08f30a2253971
mk-pmb <mk-pmb@users.noreply.github.com>
2024-06-29 20:00:07 +0200

    v1.0.0: Works for me.

---
+++ README.md
@@ -0,0 +1,110 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+easydav-pwshput-pmb
+===================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+PowerShell script to upload (HTTP PUT) files onto a WebDAV or regular HTTP
+server.
+<!--/#echo -->
+
+The script tries to upload all given files to all given servers.
+If an upload fails, the server is considered dead and the script continues
+with the next server, if any remain.
+The mission is considered successful if at least one server confirmed
+receiving all the files.
+Even then, all remaining servers will be tried, to achieve maximum spread
+of whatever is to be published.
+
+If you need another strategy, give just one file and/or just one server
+per invocation and manage the queue yourself.
+
+
+
+Installation
+------------
+
+#### Ubuntu
+
+You'll need to install [PowerShell][pwsh-rls].
+To fix potential missing dependencies after installing the .deb,
+run: `sudo apt-get install --fix-broken`
+
+  [pwsh-rls]: https://github.com/PowerShell/PowerShell/releases
+
+
+
+#### Windows 10/11
+
+The script should work out of the box.
+
+
+
+Configuration
+-------------
+
+#### Environment variables
+
+* `HTTP_CONNECT_TIMEOUT`, `HTTP_RESPONSE_TIMEOUT`:
+  Optional, in seconds, supports decimal fractions.
+* `HTTP_PUT_DATE_SUFFIX`:
+  Optional, string, add date to end of file basename (i.e. before final dot).
+  There are some pre-defined shorthands that you can use as part of your
+  date format template.
+  Search the script for the option name for more information.
+
+
+
+Usage
+-----
+
+#### CLI arguments
+
+* CLI arguments that contain `://` are destination servers.
+* All other arguments are filenames to be uploaded.
+
+
+
+#### Ubuntu
+
+```text
+./http_put.ps1 http://example.net/webdav/ http://upload.test/ package.json README.md
+```
+
+
+
+#### Windows
+
+```text
+set pwsh=powershell.exe -ExecutionPolicy Bypass -File
+%pwsh% http_put.ps1 http://example.net/webdav/ http://upload.test/ package.json README.md
+```
+
+* `-ExecutionPolicy` seems to be a type of noob protection to prevent easy
+  double-click execution of email attachments, similar to the
+  "I trust this file" checkbox that you have to set on downloaded ZIP files
+  before unpacking to make it so you can actually execute files.
+
+
+
+<!--#toc stop="scan" -->
+
+
+
+Known issues
+------------
+
+* Needs more/better tests and docs.
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ http_put.ps1
@@ -0,0 +1,94 @@
+#!/usr/bin/env pwsh
+# -*- coding: utf-8, tab-width: 2 -*-
+
+function fail($msg) { Write-Host "E: $msg"; exit 2; }
+
+$destBaseUrls = @()
+$files = @()
+
+foreach ($arg in $args) {
+  if ($arg -match '://') { $destBaseUrls += $arg; continue; }
+  $files += $arg
+}
+
+
+function env_float([string] $key, [float] $def) {
+  $val = [float][Environment]::GetEnvironmentVariable($key)
+  # Write-Host "env[$key] || ${def}: '$val'"
+  # $val = if ($val -gt 0) { $val } else { $def }
+  if ($val -gt 0) { return $val }
+  return $def
+}
+
+$timeout_conn_sec = env_float 'HTTP_CONNECT_TIMEOUT' 3
+$timeout_resp_sec = env_float 'HTTP_RESPONSE_TIMEOUT' ($timeout_conn_sec * 10)
+
+if (-not $destBaseUrls) { fail 'No base URLs given!' }
+if (-not $files) { fail 'No input filenames given.' }
+
+
+function uploadOneFileToOneServer($baseUrl, $fileName) {
+  $uploadAs = [System.IO.Path]::GetFileName($fileName)
+
+  $dateSuf = $env:HTTP_PUT_DATE_SUFFIX
+  if ($dateSuf) {
+    # To try date formats: `pwsh -Command Get-Date -Format yyMMdd-HHmmss`
+    $dateSuf = $dateSuf.Replace('?ym', 'yyMMdd-HHmm')
+    $dateSuf = $dateSuf.Replace('?ys', 'yyMMdd-HHmmss')
+    $uploadAs = "$([System.IO.Path]::GetFileNameWithoutExtension($uploadAs))$(
+      Get-Date -Format "$dateSuf")$([System.IO.Path]::GetExtension($uploadAs))"
+  }
+
+  $destUrl = New-Object System.Uri($baseUrl + $uploadAs)
+  Write-Host "D: Uploading: $fileName -> $destUrl"
+  $req = [System.Net.HttpWebRequest]::Create($destUrl)
+  $req.Method = 'PUT'
+  # Write-Host "D: Setting connect timeout to $($timeout_conn_sec * 1e3) ms"
+  $req.Timeout = $timeout_conn_sec * 1e3
+  # Write-Host "D: Setting response timeout to $($timeout_resp_sec * 1e3) ms"
+  $req.ReadWriteTimeout = $timeout_resp_sec * 1e3
+  $fileSize = (Get-Item $fileName).Length
+  $req.ContentLength = $fileSize
+  # Write-Host "D: Uploading $fileSize bytes."
+
+  $body = $req.GetRequestStream()
+  $fileStream = [System.IO.File]::OpenRead($fileName)
+  $bytesCopied = $fileStream.CopyTo($body)
+  if ($bytesCopied -ne $fileSize) {
+    # throw "Incomplete upload: Sent $bytesCopied of $fileSize bytes."
+  }
+  $fileStream.Close()
+  $body.Close()
+
+  try {
+    $rsp = $req.GetResponse()
+    Write-Host "D: Success: Uploaded $fileName -> $destUrl"
+  }
+  catch {
+    Write-Error "W: Request failed: $($_.Exception.Message)"
+  }
+  finally {
+    $rsp.Dispose()
+  }
+}
+
+
+$fullDeliveries = 0
+
+foreach ($baseUrl in $destBaseUrls) {
+  try {
+    foreach ($fileName in $files) {
+      uploadOneFileToOneServer $baseUrl $fileName
+    }
+    $fullDeliveries += 1
+  } catch {
+    Write-Error "W: Had failures for server '$baseUrl': $($_.Exception.Message)"
+  }
+}
+
+
+if ($fullDeliveries -eq 0) {
+  fail "Failed to fully deliver all files to any server."
+} else {
+  Write-Output "D: Success: Uploaded all files to $fullDeliveries server(s)."
+}
